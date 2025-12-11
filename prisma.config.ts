9113b4da20034e3ac4d6645f37652faf136a0d32
beki-kel <bereket.kelay@aait.edu.et>
2025-12-11 18:04:04 +0300

    fixed the prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
-
-console.log('🔍 Prisma Config: DATABASE_URL loaded:', process.env.DATABASE_URL ? '✅ Found' : '❌ Missing');
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
     schema: 'prisma/schema.prisma',
-    datasource: {
-        url: process.env.DATABASE_URL!,
+    migrations: {
+        path: 'prisma/migrations',
     },
-
-});
+    datasource: {
+        url: env('DATABASE_URL'),
+    },
+})
